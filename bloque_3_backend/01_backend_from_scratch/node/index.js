86aed23ef79568e8c6f178e79a7e24edd25ef855
Andres M. Prieto <andresprieto2703@gmail.com>
2023-06-30 23:38:41 -0500

    MLD
    Partiendo los archivos de la primera sesión de backend

---
+++ bloque_3_backend/01_backend_from_scratch/node/index.js
@@ -0,0 +1,18 @@
+//Este tipo de variable es "mutable", es decir; puede cambiar el tipo de dato
+let variableCambiante
+
+// Este tipo de variable es constante, su tipo de dato no se puede alterar
+const variableConstante = 0
+
+// Arrow function
+const example = () => {}
+// Así se invoca
+example()
+
+// "Normal" or "Classic" function
+function example2 () { 
+    console.log("hola")
+}
+// Así se invoca
+example2()
+
